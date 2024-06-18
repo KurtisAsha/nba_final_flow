cc39c39d3694d6a7931a684504218e340884d168
Kurtis Smith <kurtisasha@gmail.com>
2024-06-18 17:27:53 +0100

    prototype process map

--- Functions/data-transformations.R
+++ Functions/data-transformations.R
@@ -1,9 +1,9 @@
 
 # Gets dates from scorebox ------------------------------------------------
 
-get_scorebox_date <- function(play_by_play_html){
+get_scorebox_date <- function(pbp_html){
 
-play_by_play_html %>% 
+ pbp_html %>% 
  html_nodes("div.scorebox_meta") %>% 
  html_children() %>% 
  pluck(1) %>% 
@@ -13,16 +13,19 @@ play_by_play_html %>%
 }
  
 # Retrieves the rows for each quarter -------------------------------------------------------------
-get_quarter_row_indicies <- function(raw_pbp){
- 
- raw_pbp
- 
+get_quarter_row_indicies <- function(pbp_raw){
+
  # Get row references for quarters
+ quarter_1_row_location = 3:(which(pbp_raw$X1 == "2nd Q") -2)
+ quarter_2_row_location = (max(quarter_1_row_location) +5):(which(pbp_raw$X1 == "3rd Q") -2)
+ quarter_3_row_location = (max(quarter_2_row_location) +5):(which(pbp_raw$X1 == "4th Q") -2)
+ quarter_4_row_location = (max(quarter_3_row_location) +5):(nrow(pbp_raw) -1)
+ 
  quarters_list <- list(
- quarter_1_row_location = 3:(which(raw_pbp$X1 == "2nd Q") -2),
- quarter_2_row_location = (max(quarter_1_row_location) +5):(which(raw_pbp$X1 == "3rd Q") -2),
- quarter_3_row_location = (max(quarter_2_row_location) +5):(which(raw_pbp$X1 == "4th Q") -2),
- quarter_4_row_location = (max(quarter_3_row_location) +5):(nrow(raw_pbp) -1)
+  quarter_1_row_location, 
+  quarter_2_row_location, 
+  quarter_3_row_location, 
+  quarter_4_row_location
  )
  
  # Get min and max rows references for each quarter
@@ -39,21 +42,21 @@ get_quarter_row_indicies <- function(raw_pbp){
 }
 
 # Filters play by play data and adds relevant quarter ---------------------------------------------------
-add_quarters_col <- function(play_by_play_raw) {
+add_quarters_col <- function(pbp_raw) {
  
- quarter_rows <- get_quarter_row_indicies(play_by_play_raw)
+ quarter_rows <- get_quarter_row_indicies(pbp_raw)
  
- q1_pbp <- play_by_play_raw[quarter_rows[1, "min"]:quarter_rows[1, "max"], ] %>%
-  mutate(quarter = "quarter_1")
+ q1_pbp <- pbp_raw[quarter_rows[1, "min"]:quarter_rows[1, "max"], ] %>%
+  mutate(quarter = "q1")
  
- q2_pbp <- play_by_play_raw[quarter_rows[2, "min"]:quarter_rows[2, "max"], ] %>%
-  mutate(quarter = "quarter_2")
+ q2_pbp <- pbp_raw[quarter_rows[2, "min"]:quarter_rows[2, "max"], ] %>%
+  mutate(quarter = "q2")
  
- q3_pbp <- play_by_play_raw[quarter_rows[3, "min"]:quarter_rows[3, "max"], ] %>%
-  mutate(quarter = "quarter_3")
+ q3_pbp <- pbp_raw[quarter_rows[3, "min"]:quarter_rows[3, "max"], ] %>%
+  mutate(quarter = "q3")
  
- q4_pbp <- play_by_play_raw[quarter_rows[4, "min"]:quarter_rows[4, "max"], ] %>%
-  mutate(quarter = "quarter_4")
+ q4_pbp <- pbp_raw[quarter_rows[4, "min"]:quarter_rows[4, "max"], ] %>%
+  mutate(quarter = "q4")
  
  pbp_with_quarters <- bind_rows(
   q1_pbp,
@@ -65,3 +68,37 @@ add_quarters_col <- function(play_by_play_raw) {
  return(pbp_with_quarters)
  
 }
+
+
+# Calculates cumulative timestamp -----------------------------------------
+# Requires scorebox dates
+
+calc_timestamp <- function(pbp_with_quarters, scorebox_dates){
+
+ scorebox_date <- as.Date(scorebox_dates)
+ 
+ pbp_with_timestamp <- pbp_with_quarters %>%
+ rename(timestamp = X1) %>%
+ mutate(
+  timestamp = lubridate::ymd_hms(
+   paste0(scorebox_date, " 0:", timestamp)
+  ),
+  # Calculate time difference using timestamp
+  timediff = if_else(
+   is.na(lag(timestamp, 1)),
+   as.numeric(timestamp - timestamp),
+   as.numeric(lag(timestamp, 1) - timestamp)),
+  # Calculate cumulative time difference using timediff
+  timediff = seconds(cumsum(timediff)),
+  timestamp = lubridate::ymd_hms(
+   scorebox_dates + timediff
+  )
+ )
+
+return(pbp_with_timestamp)
+
+}
+
+
+
+
--- Scripts/create-event-log.R
+++ Scripts/create-event-log.R
@@ -5,20 +5,20 @@ library(bupaverse)
 library(tidyverse)
 library(rvest)
 
+source("./Functions/data-transformations.R")
+
 # Read pbp html -----------------------------------------------------------
 
-play_by_play_html <- list(
+pbp_html <- list(
   pbp_2024_06_06 = read_html("https://www.basketball-reference.com/boxscores/pbp/202406060BOS.html") 
  ,pbp_2024_06_09 = read_html("https://www.basketball-reference.com/boxscores/pbp/202406090BOS.html")
- #,pbp_2024_06_12 = read_html("https://www.basketball-reference.com/boxscores/pbp/202406090BOS.html")
- #,pbp_2024_06_14 = read_html("https://www.basketball-reference.com/boxscores/pbp/202406090BOS.html")
- #,pbp_2024_06_17 = read_html("https://www.basketball-reference.com/boxscores/pbp/202406090BOS.html")
- #,pbp_2024_06_20 = read_html("https://www.basketball-reference.com/boxscores/pbp/202406090BOS.html")
- #,pbp_2024_06_23 = read_html("https://www.basketball-reference.com/boxscores/pbp/202406090BOS.html")
+ ,pbp_2024_06_12 = read_html("https://www.basketball-reference.com/boxscores/pbp/202406090BOS.html")
+ ,pbp_2024_06_14 = read_html("https://www.basketball-reference.com/boxscores/pbp/202406090BOS.html")
+ ,pbp_2024_06_17 = read_html("https://www.basketball-reference.com/boxscores/pbp/202406090BOS.html")
 )
 
 # Get scorebox date
-scorebox_dates <- map(play_by_play_html, ~get_scorebox_date(.x))
+scorebox_dates <- map(pbp_html, ~get_scorebox_date(.x))
 
 # Read roster html --------------------------------------------------------
 
@@ -42,89 +42,67 @@ roster_data <- map(
 
 # Transform pbp data ------------------------------------------------------
 
-play_by_play_raw <- map(
+ pbp_raw <- map(
  # Get play by player table and convert to data frame
- play_by_play_html, ~html_element(., "table") %>%
-                         html_table(header = FALSE)) %>%
+ pbp_html, ~html_element(., "table") %>%
+                         html_table(header = FALSE))
+ 
+ 
+ pbp_with_quarters <- map(
  # Select rows with times only and add quarter column
- map(~add_quarters_col(.x)) %>% 
- map(~tibble::rowid_to_column(.x, var = "order"))
- 
- 
- play_by_play_raw$pbp_2024_06_06 %>%
-  rename(timestamp = X1) %>%
-  mutate(
-   timestamp = lubridate::ymd_hms(
-    paste0(as.Date(scorebox_dates$pbp_2024_06_06), " 0:", timestamp)
-   ),
-   timediff = if_else(
-    is.na(lag(timestamp, 1)),
-    timestamp - timestamp,
-    lag(timestamp, 1) - timestamp),
-    timestamp = lubridate::ymd_hms(
-scorebox_dates$pbp_2024_06_06 + timediff
-      )
+  pbp_raw, ~add_quarters_col(.x)) %>% 
+  map(~tibble::rowid_to_column(.x, var = "order"))
+
+ pbp_with_timestamp <- pmap(
+ # Calculate cumlulative timestamp
+ list(pbp_with_quarters, scorebox_dates), ~calc_timestamp(..1, ..2))
+
+ pbp_finals <- pbp_with_timestamp %>% map(
+  ~rename(.x, 
+          DAL_play = X2, 
+          DAL_score_tally = X3, 
+          score_board = X4,
+          BOS_score_tally = X5, 
+          BOS_play = X6, 
+          )
   )
  
- 
- 
- 
- 
- 
- pbp_timestamped <-  play_by_play_raw$pbp_2024_06_06 %>%
- rename(timestamp = X1) %>%
- mutate(
-  timestamp = lubridate::ymd_hms(
-   paste0(scorebox_dates$pbp_2024_06_06, " 0:", timestamp)
-  ),
-  timediff = if_else(
-   is.na(lag(timestamp, 1)),
-   timestamp - timestamp,
-   lag(timestamp, 1) - timestamp),
-  timestamp = if_else(
-   timediff == 0,
-   timestamp,
-   lag(timestamp, 1) + timediff),
-  timestamp = case_when(
-   quarter == Q2 ~ timestamp + hours(8) + minutes(30),
-   quarter == Q3 ~ timestamp + hours(8) + minutes(42),
-   quarter == Q4 ~ timestamp + hours(8) + minutes(54),
-   TRUE ~ timestamp + hours(8) + minutes(18)
-  )
- )
-
-
-
-
-
-
-play_by_play_html$pbp_2024_06_06 %>% 
- html_nodes("div.scorebox_meta")
-
-
 # Data transformations ----------------------------------------------------
 
-# Add sequence number
-pbp_with_quarters %<%
- 
-  
-  
-  
+
 
 # Create event log --------------------------------------------------------
 
-nba_eventlog <- nba_final_2024_game_7 %>% 
- eventlog(case_id = case_id, 
-          activity_id = activity_id, 
-          activity_instance_id = activity_instance_id, 
-          lifecycle_id = lifecycle_id, 
-          timestamp = timestamp, 
-          resource_id = resource_id, 
-          order = order, 
+nba_final_2024_game_5 <- read_csv("./Data/nba_final_2024_game_5.csv", col_names = TRUE)
+
+nba_eventlog <-  eventlog(nba_final_2024_game_5,
+          case_id = "case_id", 
+          activity_id = "activity_id", 
+          activity_instance_id = "activity_instance_id", 
+          lifecycle_id = "lifecycle_id", 
+          timestamp = "timestamp", 
+          resource_id = "resource_id", 
+          order = "order", 
           validate = TRUE)
 
 
-patients
+nba_eventlog %>% 
+ process_map()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
