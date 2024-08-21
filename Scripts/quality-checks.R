30d99eb23ba669b68347e6b6dc17d04e5b3a3eed
Kurtis Smith <kurtisasha@gmail.com>
2024-08-21 13:17:19 +0100

    Added handover work

--- Scripts/quality-checks.R
+++ Scripts/quality-checks.R
@@ -1,10 +1,8 @@
 
-
 # Read data ---------------------------------------------------------------
 
 # Get data
 nba_2024_g5_pm_data <- read_csv("./Data/nba-final-2024-game-5.csv", col_names = TRUE)
-
 
 nba_2024_g5_pm_data %>% 
  filter(activity_id == "pass-to-assist") %>% 
