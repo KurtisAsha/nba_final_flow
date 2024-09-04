d9786b366f942c5370e3c629638369ecbf28e4d7
Kurtis Smith <kurtisasha@gmail.com>
2024-09-04 22:00:16 +0100

    Amended to fit for Kaggle

--- Scripts/quality-checks.R
+++ Scripts/quality-checks.R
@@ -7,3 +7,6 @@ nba_2024_g5_pm_data <- read_csv("./Data/nba-final-2024-game-5.csv", col_names = 
 nba_2024_g5_pm_data %>% 
  filter(activity_id == "pass-to-assist") %>% 
  count(resource_id, sort = TRUE, name = "assists")
+
+# Used in YAML to generate references before copy and pasting to fit with Kaggle
+# bibliography: references.bib
